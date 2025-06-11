965928d0c07c4329c5158faa89b3a0d34fbd588f
167557 <167557@5CD3441DMK>
2025-06-11 21:53:02 +0530

    11/09/2025 - from LAP

--- src/test/java/javaseleniumlearning/tests/SubmitOrderTest.java
+++ src/test/java/javaseleniumlearning/tests/SubmitOrderTest.java
@@ -1,13 +1,17 @@
 package javaseleniumlearning.tests;
 
+import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -85,5 +89,7 @@ public class SubmitOrderTest extends BaseTest {
 																												// user
 																												// login
 	}
+	
+	
 
 }
