400729a4fd537901b227af12eddb6ba5dd02a8dd
167557 <167557@5CD3441DMK.ustr.com>
2025-08-07 21:20:56 +0530

    from lap 07/08/2025

--- src/test/java/javaseleniumlearning/stepDefinitions/StepDefinitionImpl.java
+++ src/test/java/javaseleniumlearning/stepDefinitions/StepDefinitionImpl.java
@@ -1,15 +1,25 @@
 package javaseleniumlearning.stepDefinitions;
 
 import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 import javaseleniumlearning.TestComponents.BaseTest;
+import javaseleniumlearning.pageobjects.CartPage;
+import javaseleniumlearning.pageobjects.CheckoutPage;
+import javaseleniumlearning.pageobjects.ConfirmationPage;
 import javaseleniumlearning.pageobjects.LandingPage;
 import javaseleniumlearning.pageobjects.ProductCatalogue;
 
 public class StepDefinitionImpl extends BaseTest {
 	
 	public LandingPage landingpage;
+	public ProductCatalogue productcatalogue;
+	public CartPage cart;  
 	
 //	Given I landed on Ecommerce Page
 	@Given("I landed on Ecommerce Page")
@@ -18,8 +28,26 @@ public class StepDefinitionImpl extends BaseTest {
 	}
 	
 //	Given Logged in with username <name> and password <password>
-	@Given ("Logged in with username (.+) and password (.+)$")
+	@Given("^Logged in with username (.+) and password (.+)$")
 	public void Logged_in_with_username_and_password(String username, String password) {
-		ProductCatalogue productcatalouge = landingpage.loginApplication(username, password);
+		productcatalogue = landingpage.loginApplication(username, password);
+	}
+	
+//	When I add product <productname> from cart
+	@When("^I add product (.+) from cart$")
+	public void I_add_product_from_cart(String productName) {
+		List<WebElement> products = productcatalogue.getProductList();
+		productcatalogue.addProductToCart(productName);
+		
+	}
+	
+//	And checkout <productname> and submit the order
+	@When ("^checkout (.+) and submit the order$")
+	public void checkout_and_submit_the_order(String productName) {
+		cart = productcatalogue.goToCartPage();
+		boolean match = cart.checkForProductName(productName);
+		Assert.assertTrue(match);
+		CheckoutPage checkoutpage = cart.checkoutBtn();
+		ConfirmationPage confirmationpage = checkoutpage.cartCheckout("india");
 	}
 }
